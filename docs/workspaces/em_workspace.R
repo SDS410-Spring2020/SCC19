31f71afda884c9b01f4b55969a503861d518d6a1
jnkwon <jkwon@smith.edu>
2020-04-23 02:19:09 -0400

    clean up and website set up

---
+++ docs/workspaces/em_workspace.R
@@ -0,0 +1,71 @@
+library(dplyr)
+library(stringr)
+library(textdata)
+library(tidytext)
+ 
+# load in data from repo
+load("textdata.Rda")
+
+# clean data, get words alone for tidytext (stolen from Jeny...thanks Jeny!)
+df_tidy <- df_full %>% 
+  unnest_tokens(word, content) %>% 
+  anti_join(stop_words)
+
+df_tidy <- df_tidy %>%
+  group_by(ID) %>% 
+  mutate(wordplace = row_number()) %>% 
+  ungroup()
+
+# get afinn lexicon into df
+afinn_lex <- get_sentiments("afinn")
+
+# join afinn to tidy text dataset
+lex_df <- inner_join(df_tidy, afinn_lex, by = "word") %>%
+  group_by(ID) %>%
+  mutate(avg_sent = mean(value))
+
+# get sentiment per ~10 words
+sum_df <- lex_df %>%
+  group_by(ID) %>%
+  group_by(ID, index = wordplace %/% 10) %>%
+  summarise(sentiment = sum(value))
+
+
+# the whole deal with afinn is that it rates works from -5-5 for sentiment
+# look at some stuff
+# NOTE: IDs for titles are inverted...most recent are lowest numbers. Consider adjusting this for plotting.
+
+# basic blueprint for plotting absvalue sentiment value per letter
+ggplot(lex_df, aes(x = ID, y = value)) + 
+  geom_col()
+
+# basic blueprint for plotting mean 
+ggplot(lex_df, aes(x = ID, y = avg_sent)) + 
+  geom_col()
+
+# basic blueprint for plotting sentiment over course of letters (faceted)
+ex_df <- lex_df %>%
+  filter(ID %in% c(1:5))
+
+ggplot(ex_df, aes(x = wordplace, y = value)) + 
+  geom_col() + 
+  facet_wrap(~ID)
+
+# get data for length of each letter individually (number of not filler words)
+letter_length <- lex_df %>%
+  group_by(ID) %>%
+  summarise(length = max(wordplace), avg_value = mean(value))
+
+# let's look at how long the letters are relative to each other
+ggplot(letter_length, aes(x = ID, y = length)) + 
+  geom_col()
+  
+
+
+
+
+
+
+
+
+
