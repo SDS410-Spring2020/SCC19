eefb273a8bee95923c7a4a33e9c7f7af368f1043
ed4ubenspeck <edaubenspeck@gmail.com>
2020-04-26 17:24:34 -0400

    afinn analysis

--- workspaces/em_workspace.R
+++ workspaces/em_workspace.R
@@ -48,6 +48,12 @@ letter_length <- lex_df %>%
   group_by(date) %>%
   summarise(length = max(wordplace), avg_value = mean(value))
 
+# categorize based on negative, neutral, positive
+cat_df <- lex_df %>%
+  mutate(cat = ifelse(value < -2, "negative",
+                ifelse(value %in% c(-1, 0, 1), "neutral",
+                       "positive")))
+
 
 # what do we want for this analysis? 
 # length of letters over time
@@ -66,6 +72,26 @@ ggplot(sum_df, aes(x = date, y = avg_sent)) +
   geom_line() + 
   theme_light() + 
   labs(x = "Date", y = "Average Sentiment")
+
+# graph based on positive negative
+ggplot(cat_df, aes(x = date)) + 
+  geom_bar() + 
+  facet_wrap(~cat)
+
+# analysis over time in single letter
+ggplot(data = lex_df %>%
+         filter(ID == 50), aes(x = wordplace, y = value)) + 
+  geom_line()
+
+finish_df <- lex_df %>%
+  group_by(ID) %>%
+  filter(wordplace > 100) %>%
+  mutate(finish_sent = mean(value)) %>%
+  select(ID, avg_sent, finish_sent)
+
+ggplot(finish_df, aes(x = avg_sent, y = finish_sent)) + 
+  geom_point()
+
 # identify points of excessive conflict or negativity
 # identify points of excessive positivity
 
