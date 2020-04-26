60fd133d4ae62115a2105aa75aa55128f87cc3d5
jnkwon <jkwon@smith.edu>
2020-04-25 23:09:03 -0400

    Website data changes. README.

--- docs/workspaces/em_workspace.R
+++ docs/workspaces/em_workspace.R
@@ -21,27 +21,19 @@ afinn_lex <- get_sentiments("afinn")
 
 # join afinn to tidy text dataset
 lex_df <- inner_join(df_tidy, afinn_lex, by = "word") %>%
-  group_by(ID) %>%
+  group_by(ID, index = wordplace %/% 10) %>%
   mutate(avg_sent = mean(value))
 
 # get sentiment per ~10 words
 sum_df <- lex_df %>%
-  group_by(ID) %>%
-  group_by(ID, index = wordplace %/% 10) %>%
-  summarise(sentiment = sum(value))
+  group_by(date, ID) %>%
+  #group_by(date, index = wordplace %/% 10) %>%
+  summarise(sentiment = sum(value), avg_sent = mean(value))
 
 
 # the whole deal with afinn is that it rates works from -5-5 for sentiment
 # look at some stuff
 # NOTE: IDs for titles are inverted...most recent are lowest numbers. Consider adjusting this for plotting.
-
-# basic blueprint for plotting absvalue sentiment value per letter
-ggplot(lex_df, aes(x = ID, y = value)) + 
-  geom_col()
-
-# basic blueprint for plotting mean 
-ggplot(lex_df, aes(x = ID, y = avg_sent)) + 
-  geom_col()
 
 # basic blueprint for plotting sentiment over course of letters (faceted)
 ex_df <- lex_df %>%
@@ -53,13 +45,29 @@ ggplot(ex_df, aes(x = wordplace, y = value)) +
 
 # get data for length of each letter individually (number of not filler words)
 letter_length <- lex_df %>%
-  group_by(ID) %>%
+  group_by(date) %>%
   summarise(length = max(wordplace), avg_value = mean(value))
 
-# let's look at how long the letters are relative to each other
-ggplot(letter_length, aes(x = ID, y = length)) + 
-  geom_col()
-  
+
+# what do we want for this analysis? 
+# length of letters over time
+ggplot(letter_length, aes(x = date, y = length)) + 
+  geom_line() + 
+  theme_light() + 
+  labs(x = "Date", y = "Length of Letter")
+
+# sentiment of letters over time (total and average)
+ggplot(sum_df, aes(x = date, y = sentiment)) + 
+  geom_line() + 
+  theme_light() + 
+  labs(x = "Date", y = "Total Sentiment")
+# basic blueprint for plotting mean 
+ggplot(sum_df, aes(x = date, y = avg_sent)) + 
+  geom_line() + 
+  theme_light() + 
+  labs(x = "Date", y = "Average Sentiment")
+# identify points of excessive conflict or negativity
+# identify points of excessive positivity
 
 
 
