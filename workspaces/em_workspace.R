2091c6d3d2ff544d867553070610484f58038170
ed4ubenspeck <edaubenspeck@gmail.com>
2020-04-28 21:54:29 -0400

    adding afinn analysis, editing data

--- workspaces/em_workspace.R
+++ workspaces/em_workspace.R
@@ -50,7 +50,7 @@ letter_length <- lex_df %>%
 
 # categorize based on negative, neutral, positive
 cat_df <- lex_df %>%
-  mutate(cat = ifelse(value < -2, "negative",
+  mutate(cat = ifelse(value <= -2, "negative",
                 ifelse(value %in% c(-1, 0, 1), "neutral",
                        "positive")))
 
@@ -66,17 +66,18 @@ ggplot(letter_length, aes(x = date, y = length)) +
 ggplot(sum_df, aes(x = date, y = sentiment)) + 
   geom_line() + 
   theme_light() + 
-  labs(x = "Date", y = "Total Sentiment")
+  labs(x = "Date", y = "Total Sentiment") +
+  geom_smooth(method = "lm")
 # basic blueprint for plotting mean 
 ggplot(sum_df, aes(x = date, y = avg_sent)) + 
   geom_line() + 
-  theme_light() + 
-  labs(x = "Date", y = "Average Sentiment")
+  theme_minimal() + 
+  labs(x = "Date", y = "Average Sentiment") +
+  geom_smooth(method = "lm")
 
 # graph based on positive negative
-ggplot(cat_df, aes(x = date)) + 
-  geom_bar() + 
-  facet_wrap(~cat)
+ggplot(cat_df, aes(x = date, fill = cat)) + 
+  geom_bar()
 
 # analysis over time in single letter
 ggplot(data = lex_df %>%
@@ -85,12 +86,14 @@ ggplot(data = lex_df %>%
 
 finish_df <- lex_df %>%
   group_by(ID) %>%
-  filter(wordplace > 100) %>%
+  filter(wordplace > 100 & wordplace < max(wordplace) - 2) %>%
   mutate(finish_sent = mean(value)) %>%
   select(ID, avg_sent, finish_sent)
 
 ggplot(finish_df, aes(x = avg_sent, y = finish_sent)) + 
-  geom_point()
+  geom_point() +
+  geom_abline(slope = 1) + 
+  geom_smooth(method = "lm")
 
 # identify points of excessive conflict or negativity
 # identify points of excessive positivity
